246c1f3c3d03114497734d426c3b7a8539abc2e4
sa2321 <118229043+sa2321@users.noreply.github.com>
2023-11-06 19:06:43 +0000

    Update fibonacci.py

--- adt_examples/fibonacci.py
+++ adt_examples/fibonacci.py
@@ -7,8 +7,6 @@ class Fib:
         return self
 
     def __next__(self):
-      """  if self.prev > 100:
-            raise StopIteration"""
         result = self.prev
         self.prev, self.current = self.current, self.prev + self.current
         return result

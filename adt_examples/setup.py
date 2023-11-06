dfec0b132d304898d344d91dffd23a37f7ded171
sa2321 <118229043+sa2321@users.noreply.github.com>
2023-11-06 18:26:27 +0000

    Create setup.py

---
+++ adt_examples/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adt_examples',  # Package name
+    version='1.0',  # Package version
+    packages=find_packages(),  # Automatically find all packages
+    install_requires=[],
+)

4f364c20eaa74d71f6065ab0c2b380fe28120c3e
sa2321 <118229043+sa2321@users.noreply.github.com>
2023-11-06 17:48:07 +0000

    Update third.py

--- testingmycode/third.py
+++ testingmycode/third.py
@@ -1,1 +1,11 @@
+from adt_examples.deque import Deque
+
+# Create an instance of the deque with a fixed size
+deque = Deque(5)
+
+# Example usage
+deque.append(1)
+deque.append(2)
+deque.append(3)
+print(list(deque))  # Should print [1, 2, 3]
 

e82b98c954d35776e7e4aa2aa6db92283ec83918
sa2321 <118229043+sa2321@users.noreply.github.com>
2023-11-06 18:44:19 +0000

    Update __init__.py

--- adt_examples/__init__.py
+++ adt_examples/__init__.py
@@ -1,4 +1,4 @@
-from adt_examples.deque import Deque
-from adt_examples.rpcalc import RPCalc
-from adt_examples.fibonacci import Fib
+from .adt_examples.deque import Deque
+from .adt_examples.rpcalc import RPCalc
+from .adt_examples.fibonacci import Fib
 
